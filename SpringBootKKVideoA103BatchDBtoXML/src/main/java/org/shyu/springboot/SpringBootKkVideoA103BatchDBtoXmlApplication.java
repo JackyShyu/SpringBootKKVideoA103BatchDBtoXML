c3b9930850cc6f32e70fab91d40e44b1354699f8
jackyshyu <jackyshyu@gmail.com>
2018-10-16 09:55:18 -0500

    Create project

---
+++ SpringBootKKVideoA103BatchDBtoXML/src/main/java/org/shyu/springboot/SpringBootKkVideoA103BatchDBtoXmlApplication.java
@@ -0,0 +1,12 @@
+package org.shyu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKkVideoA103BatchDBtoXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKkVideoA103BatchDBtoXmlApplication.class, args);
+	}
+}
---
+++ SpringBootKKVideoA103BatchDBtoXML/src/main/java/org/shyu/springboot/rowmapper/PersonRowMapper.java
@@ -0,0 +1,24 @@
+package org.shyu.springboot.rowmapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.shyu.springboot.model.Person;
+import org.springframework.jdbc.core.RowMapper;
+
+public class PersonRowMapper implements RowMapper<Person> {
+
+	@Override
+	public Person mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+		Person person = new Person();
+		
+		person.setPersonId(resultSet.getInt("person_id"));
+		person.setFirstName(resultSet.getString("first_name"));
+		person.setLastName(resultSet.getString("last_name"));
+		person.setEmail(resultSet.getString("email"));
+		person.setAge(resultSet.getInt("age"));
+		
+		return person;
+	}
+
+}
